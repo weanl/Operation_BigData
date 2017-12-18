27b8fff04784f469fadf26e507953669cc1ba116
weanl <1975346264@qq.com>
2017-12-18 15:53:32 +0800

    modify FS/test_FS.py

--- FS/test_FS.py
+++ FS/test_FS.py
@@ -13,6 +13,29 @@ adopt some algorithm for Feature Selection
 import DataStream as DS
 import minepy as mp
 
+from scipy.stats import pearsonr
+
+
+
+def pearson(X, y):
+    scores = []
+    pvalues = []
+    for i in range(X.shape[1]):
+        score, pvalue = pearsonr(X[:, i], y)
+        scores += [score,]
+        pvalues += [pvalue,]
+    return scores, pvalues
+
+
+
+
+
+
+
+
+
+
+
 
 
 
