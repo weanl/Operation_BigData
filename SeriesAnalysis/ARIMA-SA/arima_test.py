ead0eb14c09bcb91e5291e68e92f777844c5f728
weanl <1975346264@qq.com>
2017-12-25 23:27:41 +0800

    Try ACF

--- SeriesAnalysis/ARIMA-SA/arima_test.py
+++ SeriesAnalysis/ARIMA-SA/arima_test.py
@@ -20,20 +20,64 @@ import matplotlib.pyplot as plt
 import pylab as pl
 
 
-t = np.arange(1, 100)
+def ACF(S, LEN):
 
-T = 20
-PI = 3.14
-w = 2*PI/T
+    return acf(S, nlags=LEN)
 
-S = np.cos(w*t)
-R = acf(S, nlags=len(t))
-print(len(R))
+def CycleLeftShift(S, n):
 
-plot1 = pl.plot(t, R)
+    L = len(S)
+    if n > L:
+        m,n = divmod(n, L)
+        print("converted to "+str(n))
+    temp = np.zeros(n)
+    for i in range(n):
+        temp[i] = S[i]
 
-pl.show()
+    for i in range(L-n):
+        S[i] = S[i+n]
 
+    for i in range(n):
+        S[L-n+i] = temp[i]
+
+
+
+def myACF(S, LEN):
+    R = np.zeros(LEN)
+    SS = np.zeros(LEN)
+    for i in range(LEN):
+        SS[i] = S[i]
+
+    for n in range(LEN):
+        CycleLeftShift(SS, n)
+        R[n] = S.dot(SS)
+
+    return R
+
+
+
+
+
+
+if __name__ == "__main__":
+    t = np.arange(0, 100)
+    x = np.ones(100)
+    T = 20
+    PI = 3.14
+    w = 2*PI/T
+
+    S = np.cos(w*t)
+    R = myACF(x, len(t))
+    print(len(R))
+
+    plot1 = pl.plot(t, x)
+
+    pl.show()
+
+    xx = np.arange(1,10)
+    print(xx)
+    CycleLeftShift(xx, 23)
+    print(xx)
 
 
 
